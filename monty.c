a213200e7f2ca85a8fada197b3e1403842319f85
HarakaSana11 <mwangipkpipi@gmail.com>
2024-04-01 04:38:42 -0700

    all

--- monty.c
+++ monty.c
@@ -1,73 +1,38 @@
-#ifndef __MONTY__H__
-#define __MONTY__H__
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <ctype.h>
-
+#include "monty.h"
+fclose_t _close = {NULL, NULL, NULL};
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * main - a programm who implement the push and pall opcodes
+ * @argc: the number of argument
+ * @argv: the arguments
+ * Return: Always 0 (Success)
  */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-/**
- * struct fclose_s - structure to close a file
- * @file: file
- * @tmp: strdup buffer
- * @opcode: a command
- *
- * Description: a structure meant
- * to close a file
- */
-typedef struct fclose_s
+int main(int argc, char *argv[])
 {
 	FILE *file;
-	char *tmp;
-	char *opcode;
-} fclose_t;
+	const char *file_from;
+	stack_t *stack = NULL;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"nop", nop},
+		{"swap", swap},
+		{"add", add},
+		{NULL, NULL}};
 
-extern fclose_t _close;
-
-void tokenize(stack_t **stack, instruction_t instructions[],
-		int line_number, char *line, FILE *file);
-int convert_if_int(stack_t **stack, char *tmp,
-		char *second_argument, int number, FILE *file);
-void free_stack(stack_t *stack);
-void get_opcode(stack_t **stack, instruction_t instruction[], FILE *file);
-void push(stack_t **stack, unsigned int number);
-void pall(stack_t **stack, unsigned int number);
-void pint(stack_t **stack, unsigned int number);
-void pop(stack_t **stack, unsigned int number);
-void nop(stack_t **stack, unsigned int number);
-void swap(stack_t **stack, unsigned int number);
-void add(stack_t **stack, unsigned int number);
-
-#endif
+	if (argc != 2)
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	file_from = argv[1];
+	file = fopen(file_from, "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_from);
+		exit(EXIT_FAILURE);
+	}
+	_close.file = file;
+	get_opcode(&stack, instructions, file);
+	fclose(file);
+	free_stack(stack);
+	return (0);
+}
--- pall.c
+++ pall.c
@@ -1,23 +1,22 @@
 #include "monty.h"
 
 /**
- * pint - print element at the top of the stack
- * @stack: stack containing elements
- * @line_number: number of line
+ * pall- prints all the values on the stack
+ * @stack: stack to fill
+ * @line_number: line number
  */
 
-void pint(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number)
 {
+	(void) line_number;
 	stack_t *node;
 
 	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		fclose(_close.file);
-		free(_close.tmp);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
+		return;
 	node = *stack;
-	fprintf(stdout, "%d\n", node->n);
+	while (node != NULL)
+	{
+		fprintf(stdout, "%d\n", node->n);
+		node = node->next;
+	}
 }
