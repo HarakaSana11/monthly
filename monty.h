d1b3bbca97450c2c887489fe72a1ddd807ac6f73
HarakaSana11 <mwangipkpipi@gmail.com>
2024-04-01 04:54:57 -0700

    all' git push

--- monty.h
+++ monty.h
@@ -1,13 +1,11 @@
-#ifndef __MONTY__H__
-#define __MONTY__H__
-
+#ifndef MONTY_H
+#define MONTY_H
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -15,7 +13,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -25,12 +23,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -38,36 +36,20 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * struct fclose_s - structure to close a file
- * @file: file
- * @tmp: strdup buffer
- * @opcode: a command
- *
- * Description: a structure meant
- * to close a file
- */
-typedef struct fclose_s
-{
-	FILE *file;
-	char *tmp;
-	char *opcode;
-} fclose_t;
-
-extern fclose_t _close;
-
-void tokenize(stack_t **stack, instruction_t instructions[],
-		int line_number, char *line, FILE *file);
-int convert_if_int(stack_t **stack, char *tmp,
-		char *second_argument, int number, FILE *file);
-void free_stack(stack_t *stack);
-void get_opcode(stack_t **stack, instruction_t instruction[], FILE *file);
-void push(stack_t **stack, unsigned int number);
-void pall(stack_t **stack, unsigned int number);
-void pint(stack_t **stack, unsigned int number);
-void pop(stack_t **stack, unsigned int number);
-void nop(stack_t **stack, unsigned int number);
-void swap(stack_t **stack, unsigned int number);
-void add(stack_t **stack, unsigned int number);
-
+void _pint(stack_t **head, unsigned int line_num);
+void _pall(stack_t **head, unsigned int i __attribute__ ((unused)));
+void _push(stack_t **head, unsigned int line_num __attribute__ ((unused)));
+void _pop(stack_t **head, unsigned int line_num);
+void _swap(stack_t **head, unsigned int line_num);
+void _add(stack_t **head, unsigned int line_num);
+void _sub(stack_t **head, unsigned int line_num);
+void _nop(stack_t **head, unsigned int i);
+void _div(stack_t **head, unsigned int line_num);
+void _mul(stack_t **head, unsigned int line_num);
+void _mod(stack_t **head, unsigned int line_num);
+void _free(stack_t *head);
+int _isdigit(char *str);
+void _pchar(stack_t **head, unsigned int line_num);
+void _pstr(stack_t **head, unsigned int i __attribute__ ((unused)));
+extern char *tokennum;
 #endif
