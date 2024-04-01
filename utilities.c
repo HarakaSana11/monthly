62ca17cc07f11883d375f28d2607bbecab2b5435
HarakaSana11 <mwangipkpipi@gmail.com>
2024-04-01 05:09:18 -0700

    all

--- utilities.c
+++ utilities.c
@@ -1,71 +1,28 @@
 #include "monty.h"
 /**
- *_isdigit - decimal-digit character test
- *@str: pointer toa string to check
+ *_atoi - changes characters to an integer
+ *@token: number to convert
  *
- *Return: 1 if it"s an ineteger string, 0 if it's not
+ *return: converted number
  */
-int _isdigit(char *str)
+int _atoi(char *token)
 {
+	int number = 0;
 	int i = 0;
-
-	if (str[0] == '-')
-		i++;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			i++;
-		else
-			return (0);
-	}
-	return (1);
-}
-
-/**
- *_atoi - own atoi
- *@number: number to convert
- *
- *Return: converted int
- */
-int _atoi(char *number)
-{
-	int value = 0;
 	int boo = 0;
-	int i = 0;
 
-	if (number[i] == '-')
+	if (token[i] == "-")
 	{
 		boo = 1;
 		i++;
 	}
-	while (number[i] != '\0')
+	while(token[i] != '\0')
 	{
-		if (!isdigit(number[i]))
-			return (0);
-
-		value = value * 10 + number[i] - '0';
+		number = number*10 + token[i] - '0';
 		i++;
 	}
 	if (boo == 1)
-	{
-		value *= -1;
-	}
-	return (value);
+		number *= -1;
 
-}
-
-/**
- * _free - function frees all memory stored in heap
- *@head: pointer to head of linked list
- */
-void _free(stack_t *head)
-{
-	stack_t *tmp;
-
-	while (head)
-	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
+	return (number);
 }
